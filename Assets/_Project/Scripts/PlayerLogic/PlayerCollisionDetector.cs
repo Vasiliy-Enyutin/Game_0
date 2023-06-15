f219fd483f3e117c60ba41822eca4bfdcdab5cdd
Vasiliy-Enyutin <vasiliyqqerty@gmail.com>
2023-06-15 11:16:52 +0300

    Изменил камеру, добавил Cinemachine

--- Assets/_Project/Scripts/PlayerLogic/PlayerCollisionDetector.cs
+++ Assets/_Project/Scripts/PlayerLogic/PlayerCollisionDetector.cs
@@ -1,43 +1,9 @@
-using _Project.Scripts.Resources;
 using UnityEngine;
 
 namespace _Project.Scripts.PlayerLogic
 {
-    [RequireComponent(typeof(ResourceMiner))]
-    [RequireComponent(typeof(ItemCollector))]
     public class PlayerCollisionDetector : MonoBehaviour
     {
-        private ResourceMiner _resourceMiner = null!;
-        private ItemCollector _itemCollector = null!;
-
-        private void Awake()
-        {
-            _resourceMiner = GetComponent<ResourceMiner>();
-            _itemCollector = GetComponent<ItemCollector>();
-        }
-
-        private void OnTriggerStay(Collider other)
-        {
-            if (other.TryGetComponent(out ResourceItem resourceItem))
-            {
-                _itemCollector.CurrentItemForCollecting = resourceItem;
-            }
-            else if (other.TryGetComponent(out Resource resource)) 
-            {
-                _resourceMiner.CurrentResourceForMining = resource;
-            }
-        }
-
-        private void OnTriggerExit(Collider other)
-        {
-            if (other.TryGetComponent(out ResourceItem resourceItem))
-            {
-                _itemCollector.CurrentItemForCollecting = null;
-            }
-            else if (other.TryGetComponent(out Resource resource)) 
-            {
-                _resourceMiner.CurrentResourceForMining = null;
-            }
-        }
+        
     }
 }
